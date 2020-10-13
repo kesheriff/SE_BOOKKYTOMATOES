e0ffa76ca1b1e892477729ba5d13adedaf4997c0
kesheriff <bookkesheriff@gmail.com>
2020-10-13 22:51:48 +0700

    callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {
-     :omniauth_callbacks => "users/omniauth_callbacks" }
+     :omniauth_callbacks => "https://secure-temple-87364.herokuapp.com/users/auth/facebook/callback" }
   resources :movies
   root "movies#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
