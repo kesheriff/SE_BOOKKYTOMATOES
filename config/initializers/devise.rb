9ffaa2a00b59e4ee68107199e54bf22110504631
kesheriff <bookkesheriff@gmail.com>
2020-10-13 21:07:41 +0700

    facebook callback

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,7 +273,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
-  config.omniauth :facebook, "1068367063561707", "58484bd102a4605a6ee25331941d7346", callback_url: "http://localhost:3000/users/auth/facebook/callback"
+  config.omniauth :facebook, "1068367063561707", "58484bd102a4605a6ee25331941d7346", callback_url: "https://secure-temple-87364.herokuapp.com/users/auth/facebook/callback"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
