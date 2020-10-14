030f1dcc1738e2dc0e80771050ebb6d178793fff
kesheriff <bookkesheriff@gmail.com>
2020-10-14 18:48:30 +0700

    layout check user

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :movies,:tmdb_search
   root to: 'movies#index'
   get 'search' => 'movie_from_tmdb#index' , as:'search'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
